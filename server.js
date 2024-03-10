8705977df03fc24775563d40062d21030d050e96
jteleha <telehajoel@gmail.com>
2024-03-10 18:34:15 -0400

    Added function to write notes

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+const readNotesFromFile = () => {
+    const data =fs.readFileSync(path.join(__dirname, 'db.json'), 'utf8');
+    return JSON.parse(data);
+};
+
+const writeNotesToFile = (notes) => {
+    fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notes));
+};
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
