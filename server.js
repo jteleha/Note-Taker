28d7a001a1dee875dd548fd4b914fdb19c636b46
jteleha <telehajoel@gmail.com>
2024-03-10 18:03:09 -0400

    Added server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`)
+});
