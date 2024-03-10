9b5734d1b1f54b1e807fb4a7376ca4c7367fe291
jteleha <telehajoel@gmail.com>
2024-03-10 18:38:37 -0400

    Updated route to get notes

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ const writeNotesToFile = (notes) => {
     fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notes));
 };
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/api/notes', (req, res) => {
+    const notes = readNotesFromFile();
+    res.json(notes);
 });
 
 app.get('/notes', (req, res) => {
